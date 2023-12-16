3fe574a15ef10f0b24c2defa0b7eb025f563b829
unknown <anggunningastiti02@gmail.com>
2023-12-16 19:11:28 +0700

    penambahan profil

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,7 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-auth-ktx:22.3.0")
 
